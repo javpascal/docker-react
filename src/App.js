5a730e1406949b130595f207698b0319e73b185c
Javier Pastrana <jpastrana@fb.com>
2021-02-04 16:50:17 +0800

    changed app text - v1 (changed wrong place before)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here I added some text for "feature" branch.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is new text from Feature branch
+          Learn React
         </a>
       </header>
     </div>
